a400e4c357535296bd5d24e3e02f639c73706355
RubanDenis <47869184+RubanDenis@users.noreply.github.com>
2021-02-26 09:42:21 +0300

    Add pagination

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -9,6 +9,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class ListComponent implements OnInit {
   requestResult: any;
   error: boolean = false;
+  paginationPage: number = 1;
+  paginationPageSize: number = 10;
 
   constructor(private http: HttpClient) { }
 
@@ -16,7 +18,7 @@ export class ListComponent implements OnInit {
     const access_token: string = localStorage.getItem('access_token');
     const requestUrl: string = 'https://core.nekta.cloud/api/device/metering_devices';
     const requestBody: object = {
-      page: 1, last_page: 0, sort_field: "id", sort: "desc", search_string: null, device_state: "all", is_archived: false, paginate: true,
+      page: 1, last_page: 0, sort_field: "id", sort: "desc", search_string: null, device_state: "all", is_archived: false, paginate: false,
       append_fields: ["active_polling", "attributes", "tied_point"], per_page: 10
     };
     const headers = new HttpHeaders({'Authorization': `Bearer ${access_token}` });
@@ -26,7 +28,6 @@ export class ListComponent implements OnInit {
       .subscribe((result) => {
         this.error = false;
         this.requestResult = result;
-        console.log(result);
       },
         error => this.error = error
       );
